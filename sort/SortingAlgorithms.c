619156b172ee524219cbb3fe6d6f92bae5dae908
awseale <awseale@email.arizona.edu>
2017-05-18 13:40:04 -0700

    added Insertion sort

--- sort/SortingAlgorithms.c
+++ sort/SortingAlgorithms.c
@@ -41,7 +41,23 @@ void bubbleSort(int* array,int arrayLength){
   //then traverses back until its proper place is found, inserting
   //the element in its proper place
 void insertionSort(int* array,int arrayLength){
-
+     //start at the 1st element instead of the 0th because the 0th
+     //element is considered to be in order, as it is compared to nothing
+     //else at this point
+   int i,j;
+   for(i=1;i<arrayLength;i++){
+        //an element is out of place, must insert it into its correct place
+        //in the array
+      if(*(array+i) > *(array+i-1)){
+         int temp = *(array+i);
+         j = i;
+         while(temp > *(array+j)){
+            *(array+j) = *(array+j-1);
+            j--;   
+         }
+         *(array+j) = temp;
+      } 
+   }
 }
 
   //Purpose: sorts an array using the Merge Sort algorithm
@@ -55,7 +71,8 @@ void mergeSort(int* array,int arrayLength){
   //Purpose: sorts an integer array using the Quick Sort algorithm
   //picks a pivot in the array and sorts all elements in the array into
   //greater than or less than said pivot
-  //it then recurses down into each half of the array performing the same action
+  //it then recurses down into each half of the array performing the same 
+  //action
   //utilizes "Divide-and-conquer" methodology
 void quickSort(int* array,int arrayLength){
 
